1fb476a0edd5e414a839cfcf9cc2a4ce34c7acc8
a01200356 <a01200356@gmail.com>
2015-09-11 12:07:07 -0500

    Ya inserta módulos en la base de datos.

--- routes/Modulos/crearModulos.js
+++ routes/Modulos/crearModulos.js
@@ -1,14 +1,27 @@
 var express = require('express');
+var mysql = require('mysql');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-	res.send('Módulo POST.');
-	res.render('agregamodulo', { title: 'Agrega Módulo' });
+	res.render('Modulos/crearModulos', { title: 'Agrega Módulo' });
 });
 
 router.post('/', function(req, res) {
-	//var nombre = req
-	res.send('Módulo POST.');
+	var nombre = req.body.nombre;
+	var codigo = req.body.codigo;
+
+	// Prepara consulta y la ejecuta.
+	var sql = 'INSERT INTO Modulos(usuarioAdministrador, nombre, numeroModulo) VALUES(2,?,?);';
+	var params = [nombre, codigo];
+	sql = mysql.format(sql, params);
+	req.bd.query(sql, function(err) {
+		if(err) {
+			console.log(err);
+			res.end('Error: No se pudo crear módulo ' + nombre + '.');
+		} else {
+			res.end('Felicidades: Módulo ' + nombre + " se ha creado exitosamente.");
+		}
+	});
 });
 
 module.exports = router;
--- routes/Modulos/modulos.js
+++ routes/Modulos/modulos.js
@@ -4,13 +4,7 @@ var router = express.Router();
 var app = express();
 var crearModulos = require('./crearModulos');
 
-router.get('/', function(req, res) {
-  //res.render('agregamodulo', { title: 'Agrega Módulo' });
-  res.send('Módulo de módulos.');
-});
+//app.use('/', );
+app.use('/nuevo', crearModulos);
 
-//router.get('/crear', crearModulos.get);
-
-app.use('/crear', crearModulos);
-
-module.exports = router;
+module.exports = app;
