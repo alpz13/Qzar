c98ff6df0e15b99fe38fe144ee0e3a159ba8a93d
Alejandro López Portillo Zabre <alpz13_2@hotmail.com>
2015-09-11 23:11:19 -0500

    se agrego la carpeta database en la raiz con database/credencialesdb.js que contiene las credenciales de la base de datos dentro de routes se agrego la carpeta Actividades que manejara todas las acciones relacionadas a actividades en la carpeta de actividades contiene listarActividades.js que hace explicitamente lo que dice se agrego en carpeta View la interfaz actividades.jade que muestra la lista de actividades.jade en index.js se agrego la ruta de /actividades

---
+++ routes/Actividades/listarActividades.js
@@ -0,0 +1,22 @@
+var mysql = require('mysql');
+var credenciales = require('../../database/credencialesdb.js')
+
+var listarActividades = function(res){
+	var db =mysql.createConnection(credenciales);
+	db.connect();
+	db.query('Select * from actividades',function(err, rows){
+		if(err){
+			console.log("Sucedio el error" + err);
+			db.end();
+		}
+		db.end();
+		res.render('actividades', {
+			title: 'Actividades',
+			actividades: rows
+		});
+	});
+}
+
+module.exports = {
+	'listarActividades' : listarActividades
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var listar = require('./Actividades/listarActividades.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/actividades', function(req, res, next){
+	listar.listarActividades(res);
 });
 
 module.exports = router;
