02f3daa068828f22caabe5faedef613a1ad19a9f
Alejandro López Portillo Zabre <alpz13_2@hotmail.com>
2015-09-15 18:40:39 -0500

    Agregado bootstrap, se volvio responsivo, auto size, menu, jquery y algunas cosas mas

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
 router.get('/actividades', function(req, res, next){
 	listar.listarActividades(res);
 });
