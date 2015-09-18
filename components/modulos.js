74a02850d5593dfef43ce06de18f82b3aea4b317
Rafa <rafa.uresti@gmail.com>
2015-09-18 17:07:12 -0500

    Validación de campos

--- components/modulos.js
+++ components/modulos.js
@@ -3,11 +3,32 @@ var mysql = require('mysql');
 var router = express.Router();
 
 var credenciales = require('../database/credencialesbd.json');
+function isEmpty(str) {
+    return (!str || 0 === str.length);
+}
 
 function crearModulo(nombre, codigo, callback) {
-
 	var bd = mysql.createConnection(credenciales);
 	bd.connect();
+	/*
+	var sql2="SELECT nombre FROM modulos where nombre = ?";
+	var params2 = [nombre];
+	sql2 = mysql.format(sql2, params2);
+	bd.query(sql2, function(err, data) {
+		if(err) {
+			bd.end();
+			return callback(err);
+		} else {
+			//console.log(data.legth);
+			if(data.length > 0)
+			{
+				//console.log('El modulo ya existe');
+				return callback('El modulo ya existe');
+			}
+		}
+	});
+	*/
+	
 
 	// Prepara consulta y la ejecuta.
 	// POR HACER:
--- routes/modulos.js
+++ routes/modulos.js
@@ -3,15 +3,31 @@ var router = express.Router();
 
 var modulos = require('../components/modulos.js');
 
+function isEmpty(str) {
+    return (!str || 0 === str.length);
+}
+
 router.get('/nuevo', function(req, res, next) {
 	res.render('crearmodulos', { title: 'Nuevo módulo' });
 });
 
-router.post('/nuevo', function(req, res, next) {
+router.post('/nuevo', function(req, res, next) 
+{
+	if(isEmpty(req.body.nombre) || isEmpty(req.body.codigo) ) 
+	{
+		res.end('Los campos no pueden estar vacios.');
+	}
 	modulos.crear(req.body.nombre, req.body.codigo, function(err) {
 		if (err) {
-			console.log(err);
-			res.end('Error: No se puedo crear módulo.');
+			//res.end(err);
+			//res.render(err);
+			//console.log(err.code);
+
+			if (err.code == 'ER_DUP_ENTRY') {
+				res.end('El Módulo/código ya existe.');
+			}
+			
+			//res.end('Error: No se puedo crear módulo.');
 		} else {
 			res.end('Felicidades: Módulo ' + req.body.nombre + ' creado exitosamente.');
 		}
