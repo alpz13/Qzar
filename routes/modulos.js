5eb27b45467341171f68a4eecf5f19e713da1dd0
a01200356 <a01200356@gmail.com>
2015-09-20 00:00:38 -0500

    Que código siempre sí es un número.

--- routes/modulos.js
+++ routes/modulos.js
@@ -10,7 +10,7 @@ var modulos = require('../components/modulos.js');
 
 // Página principal de módulos
 router.get('/', function (req, res, next) {
-    res.render('modulos', { titulo: 'Módulos', mensaje: req.mensaje });
+    res.render('modulos', { titulo: 'Módulos' });
 });
 
 // Formulario para crear un nuevo módulo.
@@ -22,27 +22,25 @@ router.get('/nuevo', function (req, res, next) {
 router.post('/nuevo', function (req, res, next) {
     var mensaje;
 
-    // Verifica que el nombre y el código de módulo no sean vacíos.
-    if (req.body.nombre.match(/^\s*$/) || req.body.codigo.match(/^\s$/)) {
+    // Verifica que el nombre y el número de módulo no sean vacíos.
+    if (req.body.nombre.match(/^\s*$/) || req.body.numero.match(/^\s$/)) {
         res.render('crearmodulos', { titulo: 'Nuevo módulo', error: 'Los campos no pueden estar vacíos.' });
         return;
     }
 
     // Intenta crear módulo.
-    modulos.crear(req.body.nombre, req.body.codigo, function (err) {
+    modulos.crear(req.body.nombre, req.body.numero, function (err) {
         // Si hubo error, regresa al formulario de nuevo módulo con el mensaje de error correspondiente.
         if (err) {
             if (err.code === 'ER_DUP_ENTRY') {
-                mensaje = 'El módulo/código ya existe.';
+                mensaje = 'Un módulo con este nombre o con este número ya existe.';
             } else {
                 mensaje = 'Hubo un error al crear el nuevo módulo. Inténtelo más tarde.';
             }
             res.render('crearmodulos', { titulo: 'Nuevo módulo', error: mensaje });
 
         } else {
-            mensaje = 'Felicidades: Módulo ' + req.body.nombre + ' creado exitosamente.';
             res.redirect('/modulos');
-            //res.render('modulos', { titulo: 'Módulos', mensaje: mensaje});
         }
     });
 });
