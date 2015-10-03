a08774027e64e57649eb24f4af53470d2bd1beae
Miguel <maap93@outlook.com>
2015-10-03 18:35:55 -0500

    Arreglo mergeado

--- routes/formularioCrearHuerta.js
+++ routes/formularioCrearHuerta.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.get('/', function (req, res, next) {
  /* res.send('respond with a resource');*/
-  res.render('formularioCrearHuerta', {title: 'Form'});
+  res.render('formularioCrearHuerta', {title: 'Form', usuario: req.session.usuario});
 });
 
 
