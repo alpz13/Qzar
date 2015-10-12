3c048d339a45432f2166ac480eda003ed488b34e
Miguel <maap93@outlook.com>
2015-10-12 18:30:47 -0500

    Arreglado el problema de guardar los cuadritos en la base, y villers se va a casar :3

--- routes/crearHuertaGuardar.js
+++ routes/crearHuertaGuardar.js
@@ -15,16 +15,17 @@ var insertaCuadrito = function(idModulo, idSector, cuadrito){
     } else{
       //Error al ejecutar el query
       console.log( "INSERT INTO Cuadritos (idSectores, idContenidoCuadritos, x, y) VALUES ('" + idSector + "', '" + cuadrito["type"] + "', '" + cuadrito["ejeX"] + "', '" + cuadrito["ejeY"] + "')");
-      console.log(err);
       console.log('Error while performing Query. (insertar cuadrito)');
+      console.log(err);
     }
   });
   //Termina la conexion
   connection.end();
 }
 
-var insertaSector = function(idModulo, numeroSector, cuadrito){
+var insertaSector = function(idModulo, numeroSector, cuadrito, _renglon, cuadritos){
   //Carga el modulo de mySQL
+  console.log("-----------" + numeroSector);
   var mysql = require('mysql');
   connection = creaConexion();
   //Guardar el cuadrito
@@ -39,11 +40,45 @@ var insertaSector = function(idModulo, numeroSector, cuadrito){
       //Error al ejecutar el query
       console.log("INSERT INTO Sectores (idModulos, numeroSector) VALUES ('" + idModulo + "', '" + numeroSector + "')");
       console.log('Error while performing Query. (insertar sector)');
-      //console.log(err);
+      console.log(err);
     }
+    if(cuadritos.length > _renglon+1)
+      selectSector(idModulo, cuadritos, _renglon+1)
+    console.log("+++++++++++++++++" + numeroSector);
   });
   //Termina la conexion
   connection.end();
+}
+
+var selectSector = function(idModulo, cuadritos, _renglon){
+  var cuadrito = cuadritos[_renglon];
+  connection = creaConexion();
+  connection.query("SELECT idSector from Sectores WHERE idModulos = '" + idModulo + "' AND numeroSector = '" + cuadrito['sector'] + "'", function(err, rows, fields) {
+      if (!err) {
+        var idSector;
+        if(rows.length > 0){
+          //Existe
+          console.log("Select exitoso");
+          console.log(rows[0].idSector);
+          idSector=rows[0].idSector;
+          insertaCuadrito(idModulo, idSector, cuadrito);
+          if(cuadritos.length > _renglon+1)
+            selectSector(idModulo, cuadritos, _renglon+1);
+        } else {
+          //No existe
+          console.log("Select no exitoso");
+          idSector = insertaSector(idModulo, cuadrito["sector"], cuadrito, _renglon, cuadritos);
+        }
+        
+      } else {
+        console.log('Error while performing Query. (Searching if sector exist)');
+        console.log("este es el error joven: " + err);
+        //console.log(cuadrito['type']);
+        //console.log("SELECT idSector from Sectores WHERE idModulos = '" + idModulo + "' numeroSector = '" + cuadrito['sector'] + "' ");
+      }
+    
+    });
+    connection.end();  
 }
 
 
@@ -69,39 +104,15 @@ var creaConexion = function(){
 router.post("/", function(request,response,next){
   //Carga el modulo de mySQL
   var mysql = require('mysql');
-  connection = creaConexion();
+  
   var cuadritos = JSON.parse(request.body.cuadritos);
   var idModulo = request.body.modulo;
   //Ver si existe el sector, si no crearlo
   var renglones = cuadritos.length;
   console.log("renglones: " + renglones);
-  for(var _renglon = 0; _renglon < renglones; _renglon++){
-    cuadrito=cuadritos[_renglon];
-    //Existe el sector ¿?
-    connection.query("SELECT idSector from Sectores WHERE idModulos = '" + idModulo + "' AND numeroSector = '" + cuadrito['sector'] + "'", function(err, rows, fields) {
-      if (!err) {
-        var idSector;
-        if(rows.length > 0){
-          //Existe
-          console.log("Select exitoso");
-          console.log(rows);
-          idSector=rows[0].idSector;
-          insertaCuadrito(idModulo, idSector, cuadrito);
-        } else {
-          //No existe
-          console.log("Select no exitoso");
-          idSector = insertaSector(idModulo, cuadrito["sector"], cuadrito);
-        }
-        
-      } else {
-        console.log('Error while performing Query. (Searching if sector exist)');
-        //console.log(cuadrito['type']);
-        //console.log("SELECT idSector from Sectores WHERE idModulos = '" + idModulo + "' numeroSector = '" + cuadrito['sector'] + "' ");
-      }
-    });
-  }
+  //Existe el sector ¿?
+  selectSector(idModulo, cuadritos, 0);
   //Termina la conexion
-  connection.end();
   //Redirecciona a una URL
   response.redirect('/contador');
 });
