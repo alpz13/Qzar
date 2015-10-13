7e803732c55cc6147c13648ca035b71745207ba3
Miguel <maap93@outlook.com>
2015-10-13 16:38:40 -0500

    Arreglado el bugsito de la huerta y clickeable una sola vez, Villers ya no se casa :(, los mantendre informados

--- routes/crearHuertaGuardar.js
+++ routes/crearHuertaGuardar.js
@@ -101,6 +101,22 @@ var creaConexion = function(){
 }
 
 
+var tamHuerta = function(idModulo, alto, ancho){
+  connection = creaConexion();
+  connection.query("UPDATE Modulos set alto = '"+ alto +"', ancho = '"+ ancho +"' WHERE idModulo = '"+ idModulo + "'", function(err, rows, fields) {
+      if (!err) {
+        console.log("UPDATE Modulos set alto = '"+ alto +"', ancho = '"+ ancho +"' WHERE idModulo = '"+ idModulo + "'");
+      } else {
+        console.log('Error while performing Query. (Searching if sector exist)');
+        console.log("este es el error joven: " + err);
+      }
+    
+    });
+    connection.end();  
+
+}
+
+
 router.post("/", function(request,response,next){
   //Carga el modulo de mySQL
   var mysql = require('mysql');
@@ -109,12 +125,20 @@ router.post("/", function(request,response,next){
   var idModulo = request.body.modulo;
   //Ver si existe el sector, si no crearlo
   var renglones = cuadritos.length;
-  console.log("renglones: " + renglones);
+  //Alto y ancho
+  var ancho = request.body.ancho;
+  var alto = request.body.alto;
+  tamHuerta(idModulo, alto, ancho);
+  console.log("ALto:"+ alto);
+  console.log("ancho: "+ ancho);
   //Existe el sector ¿?
   selectSector(idModulo, cuadritos, 0);
   //Termina la conexion
   //Redirecciona a una URL
-  response.redirect('/contador');
+  //response.redirect('/contador');
+  response.setHeader('Content-Type', 'application/json');
+  response.send(JSON.stringify(idModulo));
+
 });
 
 
