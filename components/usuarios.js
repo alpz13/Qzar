952452451b0eef12d7cda4e5b2e4a5df016b8727
Rafa <rafa.uresti@gmail.com>
2015-10-14 15:20:13 -0500

    Se modifico la consulta para que solo muestre los usuarios activos

--- components/usuarios.js
+++ components/usuarios.js
@@ -31,7 +31,7 @@ function listarUsuariosModulo(id, callback) {
 //listar usuarios
 function listarUsuarios(callback) {
     var bd = mysql.createConnection(credenciales),
-        sql = '(Select u.idUsuario, u.nombre as nombreUsuario, r.nombre as nombreRol, m.nombre as nombreModulo from usuarios as u, roles as r, modulos as m where u.idRoles = r.idRol and u.idModulo = m.idModulo order by m.nombre) UNION (Select u.idUsuario, u.nombre as nombreUsuario, r.nombre as nombreRol, u.idModulo as nombreModulo from usuarios as u, roles as r where u.idRoles = r.idRol and  idModulo IS NULL);';
+        sql = '(Select u.idUsuario, u.nombre as nombreUsuario, r.nombre as nombreRol, m.nombre as nombreModulo from usuarios as u, roles as r, modulos as m where u.idRoles = r.idRol and u.idModulo = m.idModulo and u.activo = 1 order by m.nombre) UNION (Select u.idUsuario, u.nombre as nombreUsuario, r.nombre as nombreRol, u.idModulo as nombreModulo from usuarios as u, roles as r where u.idRoles = r.idRol and u.activo = 1 and idModulo IS NULL);';
         //sql = '(Select u.nombre as nombreUsuario, r.nombre as nombreRol, m.nombre as nombreModulo from usuarios as u, roles as r, modulos as m where u.idRoles = r.idRol and u.idModulo = m.idModulo) UNION (Select u.nombre as nombreUsuario, r.nombre as nombreRol, u.idModulo as nombreModulo from usuarios as u, roles as r where u.idRoles = r.idRol and  idModulo IS NULL);';
     bd.connect();
     // Ejecuta consulta.
