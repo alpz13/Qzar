7d68f3ad3788be9ccd9862844dfe28c6541caecd
Rafa <rafa.uresti@gmail.com>
2015-11-07 14:32:32 -0600

    lista los lotes, pero falta el identificador del lote que manejan en qzar (no se que elementos debe contener)

--- routes/lotes.js
+++ routes/lotes.js
@@ -3,9 +3,26 @@
 */
 var express = require('express');
 var router = express.Router();
+var usuarios = require('../components/usuarios.js');
+var lotes = require('../components/lotes.js');
 
-router.get('/*', function (req, res, next) {
-    res.render('menu', {usuario: req.session.usuario, barraLateral: 'lotes', titulo: "###", aviso: {tipo: 'danger', icono: 'fa fa-frown-o', mensaje: 'Este componente aún no ha sido implementado.'}});
+// Página principal de módulos
+router.get('/', function (req, res, next) {
+    if (req.session.usuario.idRoles !== 1) {
+        res.redirect('/lotes/' + req.session.usuario.idModulo);
+        return;
+    }
+    lotes.listar(function (err, lotes) {
+        if (err) {
+            console.log(err);
+        }
+        usuarios.listarAdminsGenerales(function (err, usuarios) {
+            if (err) {
+                console.log(err);
+            }
+            res.render('lotes', { titulo: 'Lotes', lotes: lotes, usuario: req.session.usuario, listaAdmins: usuarios, barraLateral: 'lotes' });
+        });
+    });
 });
 
 module.exports = router;
