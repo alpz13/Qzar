02477cb68c9201f631449610df6dbb5316e46f2d
Rafa <rafa.uresti@gmail.com>
2015-11-07 16:21:33 -0600

    muestra la información de la lotificación especifica cuando se da click en el lote deseado

---
+++ public/js/modificarlote.js
@@ -0,0 +1,18 @@
+$(document).on("click", ".modificar", function () { 
+	var obj = (this).closest("tr");
+
+	var nombre = $(obj).find(".nombre").html();
+	var sector = $(obj).find(".sector").html();
+    var fecha = $(obj).find(".fecha").html();
+    var modulo = $(obj).find(".modulo").html();
+    var contenido = $(obj).find(".contenido").html();
+    
+    var id = $(obj).find(".id").html();
+
+    document.getElementById('MIDU').value = id;
+    document.getElementById('cosechador').value = nombre;
+    document.getElementById('sector').value = sector;
+    document.getElementById('fecha').value = fecha;
+    document.getElementById('modulo').value = modulo;
+    document.getElementById('contenido').value = contenido;
+});
