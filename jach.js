08990e5064f9fbb7f78ef6cadaa9bfb9fceeb434
a01200356 <a01200356@itesm.mx>
2015-11-12 01:31:24 -0600

    Quitar archivo que no iba acá.

--- jach.js
+++
@@ -1,9 +0,0 @@
-var b = require('bcrypt');
-b.genSalt(10, function(err, salt) {
-	b.hash('adminmatriz', salt, function(err, hash) {
-		console.log(hash);
-		b.compare('adminmatriz', hash, function(err, res) {
-			console.log(res);
-		});
-	});
-});
