5a2ae001728480dd7316f2dc0f79110a98b9636d
a01200356 <a01200356@itesm.mx>
2015-11-16 17:05:21 -0600

    Cosa con la conexión a base de datos.

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ router.get('/', function (req, res, next) {
         res.render('index');
     } else {
         res.render('menu', {usuario: req.session.usuario, aviso: {mensaje:'', tipo:'danger'}});
+		console.log(req.session.usuario);
     }
 });
 
