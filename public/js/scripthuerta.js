fdf9069057858308ba4fc8e89c451ac8b5a569b3
Guilherme C. T <goosehead101@hotmail.com>
2015-11-24 17:30:38 -0600

    Facelift exitoso, de huertas.

--- public/js/scripthuerta.js
+++ public/js/scripthuerta.js
@@ -1,14 +1,21 @@
 function rellenaCuadritos(cuadritos){
 	var huerta = $(".renglon");
 	$(cuadritos).each(function(){
+        console.log('dobabes');
+        console.log($(this)[0]);
+
 		var target = $(huerta[$(this)[0].y]).find(".cuadrito")[$(this)[0].x];
-		$(target).html($(this)[0].numeroSector);
-		$(target).attr('bgcolor', $(this)[0].color);
+        if($(this)[0].nombre != 'Vacío') {
+          $(target).html('<span class="poste"><i class="glyphicon glyphicon-grain" style="color: '+ $(this)[0].color +'"></i><strong style="color: '+ $(this)[0].color +'">'+ $(this)[0].numeroSector + '</strong></span>');
+        }
+		$(target).attr('color', $(this)[0].color);
 		$(target).attr('title', $(this)[0].nombre);
 		$(target).attr('data-toggle', "tooltip");
 		$(target).attr('data-placement', "top");
 		$(target).attr('data-container', "body");
 	});
+
+
 }
 
 $(document).ready(
