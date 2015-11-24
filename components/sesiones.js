470d62038b16c0e54b78d1ca59398607ba889a1b
a01200356 <a01200356@itesm.mx>
2015-11-23 21:20:32 -0600

    Errorcito de SQL en abrir sesión.

--- components/sesiones.js
+++ components/sesiones.js
@@ -28,7 +28,7 @@ var __obtenerPermisos = function (idUsuario, callback) {
 
 var __validarCredenciales = function (nombreUsuario, contrasenia, callback) {
 	var conexion = mysql.createConnection(credenciales);
-    var consulta = 'SELECT `contrasena` FROM `Usuarios` WHERE `nombre` = ? AND activo = 1;';
+    var consulta = 'SELECT `idUsuario`, `contrasena` FROM `Usuarios` WHERE `nombre` = ? AND activo = 1;';
     var valores = [nombreUsuario, contrasenia];
     consulta = mysql.format(consulta, valores);
 
