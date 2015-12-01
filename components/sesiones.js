b3b6110395a3d06447bd3d06bbbe213ed60beea9
a01200356 <a01200356@itesm.mx>
2015-11-30 19:10:52 -0600

    Copia de develop para que prueben cosas en Windows sin tener que instalar Microsoft Visual Studio C++ 2011 Chingadera Edition Plus Ultra. Hash de contraseñas comentadas. Si cambian algo acá y van a mergear con develop, nomás fíjense que los archivos de components/sesiones.js y components/usuarios.js guarden lo descomentado del original.

--- components/sesiones.js
+++ components/sesiones.js
@@ -3,7 +3,7 @@
 
 var mysql = require('mysql');
 var credenciales = require('../database/credencialesbd.json');
-var bcrypt = require('bcrypt');
+//var bcrypt = require('bcrypt');
 
 /*
 */
@@ -45,7 +45,8 @@ var __validarCredenciales = function (nombreUsuario, contrasenia, callback) {
         
         // Busca al usuario que tenga la contraseña dada.
         for (var i in renglones) {
-			if (bcrypt.compareSync(contrasenia, renglones[i].contrasena || ' ')) {
+			if (contrasenia === renglones[i].contrasena) {
+			//if (bcrypt.compareSync(contrasenia, renglones[i].contrasena || ' ')) {
 				callback(null, renglones[i].idUsuario);
 				return;
 			}
